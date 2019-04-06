5998e26f847a128eecc4174f7c59dd97b0ed6b76
Koutaroutarou <kw_extaro24karats@ezweb.ne.jp>
2019-04-06 16:39:54 +0900

    Initial Commit

---
+++ WordList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordList
+//
+//  Created by 渡辺航太郎 on 2019/04/06.
+//  Copyright © 2019 litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
